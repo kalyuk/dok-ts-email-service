722dc939264ebce1c68f0a887a11b284a756d9b2
Dmitriy Kalyuk <dmitry@kalyuk.ru>
2017-11-30 12:51:30 +0300

    init

---
+++ src/EmailService.ts
@@ -0,0 +1,25 @@
+import {BaseService} from 'dok-ts/base/BaseService';
+import * as nodemailer from 'nodemailer';
+
+export class EmailService extends BaseService {
+  private sender;
+  public static options = {
+    transport: {}
+  };
+
+  public init() {
+    super.init();
+    this.sender = nodemailer.createTransport(this.transport);
+  }
+
+  public send(data) {
+    return new Promise((resolve) => {
+      this.sender.sendMail(data, (err, info) => {
+        if (err) {
+          throw new Error(JSON.stringify(err));
+        }
+        resolve(info);
+      })
+    })
+  }
+}
